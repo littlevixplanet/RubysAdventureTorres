47cdadee97cb01d215d71809a95968c3ded1fe9f
Victor T <116121215+littlevixplanet@users.noreply.github.com>
2022-11-14 15:24:44 -0800

    Update
    Made an enemy, damaging spikes, and collectible health

--- RubysAdventureTorresP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureTorresP7/Assets/Scripts/RubyController.cs
@@ -6,7 +6,11 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public int health { get { return currentHealth; } }
     int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -22,6 +26,12 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
     void FixedUpdate()
     {
@@ -31,8 +41,15 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
