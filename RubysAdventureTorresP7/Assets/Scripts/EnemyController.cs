8f03670d4293a0f759892e998f761b7ee5a54066
Victor T <116121215+littlevixplanet@users.noreply.github.com>
2022-11-16 15:27:04 -0800

    Animation
    Added animation for the bot and ruby

--- RubysAdventureTorresP7/Assets/Scripts/EnemyController.cs
+++ RubysAdventureTorresP7/Assets/Scripts/EnemyController.cs
@@ -7,17 +7,23 @@ public class EnemyController : MonoBehaviour
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
+
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
     void update()
     {
         timer -= Time.deltaTime;
+
         if (timer < 0)
         {
             direction = -direction;
@@ -30,13 +36,18 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2D.position;
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed;
+            position.y = position.y + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            position.x = position.x + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         rigidbody2D.MovePosition(position);
+
     }
     void OnCollisionEnter2D(Collision2D other)
     {
