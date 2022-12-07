e3d7c96ff379426ee473c5605239218aa247424c
Victor T <116121215+littlevixplanet@users.noreply.github.com>
2022-12-07 15:23:57 -0800

    Additions
     Made UI, staring making Jambi Character

--- RubysAdventureTorresP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureTorresP7/Assets/Scripts/RubyController.cs
@@ -6,40 +6,35 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+    public GameObject projectilePrefab;
     public int health { get { return currentHealth; } }
     int currentHealth;
+    public float timeInvincible = 2.0f;
     bool isInvincible;
-    public GameObject projectilePrefab;
     float invincibleTimer;
-    Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
-    void Launch()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-
-        animator.SetTrigger("Launch");
-    }
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
         animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         Vector2 move = new Vector2(horizontal, vertical);
+
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
@@ -54,11 +49,10 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
-
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                Launch();
-            }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
     void FixedUpdate()
     {
@@ -74,10 +68,21 @@ public class RubyController : MonoBehaviour
         {
             if (isInvincible)
                 return;
+
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
